c896dcfd2d983343817ab012de9953787b51a048
Janosch <janosch84@gmail.com>
2015-10-21 12:52:11 +0200

    improved conversion_spec

--- spec/js_regex/conversion_spec.rb
+++ spec/js_regex/conversion_spec.rb
@@ -25,22 +25,20 @@ describe JsRegex::Conversion do
   describe '#perform_sanity_check' do
     context 'when there is a syntax error in the source' do
       let(:bad_conversion) do
-        conv = JsRegex::Conversion.new(/abc/)
+        conversion = JsRegex::Conversion.new(/abc/)
         # manually inject illegal source
-        conv.instance_variable_set('@source', '[')
-        conv
+        conversion.source.replace('[')
+        conversion
       end
 
       it 'sets the source to empty' do
-        bad_conversion.send(:perform_sanity_check)
-        source = bad_conversion.instance_variable_get('@source')
-        expect(source).to be_empty
+        expect { bad_conversion.send(:perform_sanity_check) }
+          .to change { bad_conversion.source }.from('[').to('')
       end
 
       it 'adds a warning' do
-        bad_conversion.send(:perform_sanity_check)
-        warnings = bad_conversion.instance_variable_get('@warnings')
-        expect(warnings).not_to be_empty
+        expect { bad_conversion.send(:perform_sanity_check) }
+          .to change { bad_conversion.warnings.count }.by(1)
       end
     end
   end
