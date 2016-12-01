02221d3a2dbac327d80d3bc0ea374d45d74b695e
Jannosch Müller <janosch.mueller@betterplace.org>
2016-12-01 22:46:48 +0100

    extract data list from large method

--- lib/js_regex/converter/escape_converter.rb
+++ lib/js_regex/converter/escape_converter.rb
@@ -11,30 +11,34 @@ class JsRegex
     class EscapeConverter < JsRegex::Converter::Base
       private
 
+      ESCAPES_SHARED_BY_RUBY_AND_JS = [
+        :backslash,
+        :bol,
+        :carriage,
+        :codepoint,
+        :dot,
+        :eol,
+        :form_feed,
+        :hex,
+        :interval_close,
+        :interval_open,
+        :newline,
+        :octal,
+        :one_or_more,
+        :set_close,
+        :set_open,
+        :tab,
+        :vertical_tab,
+        :zero_or_more,
+        :zero_or_one
+      ].freeze
+
       def convert_data
         case subtype
-        when :backslash,
-             :bol,
-             :carriage,
-             :codepoint,
-             :dot,
-             :eol,
-             :form_feed,
-             :hex,
-             :interval_close,
-             :interval_open,
-             :newline,
-             :octal,
-             :one_or_more,
-             :set_close,
-             :set_open,
-             :tab,
-             :vertical_tab,
-             :zero_or_more,
-             :zero_or_one
-          pass_through
         when :literal
           LiteralConverter.convert_data(data)
+        when *ESCAPES_SHARED_BY_RUBY_AND_JS
+          pass_through
         else
           # Bell, Escape, HexWide, Control, Meta, MetaControl, ...
           warn_of_unsupported_feature
