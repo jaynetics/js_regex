e22f755bdae009e3537780dfe31120cfca800f8a
Jannosch Müller <janosch.mueller@betterplace.org>
2017-09-18 19:23:40 +0200

    revert symbol array usage to support ruby 1.9.3

--- lib/js_regex/converter/escape_converter.rb
+++ lib/js_regex/converter/escape_converter.rb
@@ -11,28 +11,28 @@ class JsRegex
     class EscapeConverter < JsRegex::Converter::Base
       private
 
-      ESCAPES_SHARED_BY_RUBY_AND_JS = %i[
-        backslash
-        bol
-        carriage
-        codepoint
-        dot
-        eol
-        form_feed
-        group_close
-        group_open
-        hex
-        interval_close
-        interval_open
-        newline
-        octal
-        one_or_more
-        set_close
-        set_open
-        tab
-        vertical_tab
-        zero_or_more
-        zero_or_one
+      ESCAPES_SHARED_BY_RUBY_AND_JS = [
+        :backslash,
+        :bol,
+        :carriage,
+        :codepoint,
+        :dot,
+        :eol,
+        :form_feed,
+        :group_close,
+        :group_open,
+        :hex,
+        :interval_close,
+        :interval_open,
+        :newline,
+        :octal,
+        :one_or_more,
+        :set_close,
+        :set_open,
+        :tab,
+        :vertical_tab,
+        :zero_or_more,
+        :zero_or_one
       ].freeze
 
       def convert_data
