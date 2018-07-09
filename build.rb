a6bd6114d7bdd05899cc0bf68ee1bb554e6a50fa
Jannosch Müller <janosch.mueller@betterplace.org>
2018-07-09 16:37:19 +0200

    disable full build until needed deps are updated

--- build.rb
+++ build.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class JsRegex
-  PERFORM_FULL_BUILD = RUBY_VERSION.start_with?('2.5')
+  # TODO: re-enable this when mutant's regexp_parser dependency is up-to-date
+  PERFORM_FULL_BUILD = false # RUBY_VERSION.start_with?('2.5')
 end
