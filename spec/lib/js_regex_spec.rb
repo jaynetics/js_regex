e07a601b42637fd6e78613e9b60a8d9da2596065
Jannosch Müller <janosch.mueller@betterplace.org>
2021-03-03 15:55:23 +0100

    No need for #described_class here

--- spec/lib/js_regex_spec.rb
+++ spec/lib/js_regex_spec.rb
@@ -6,7 +6,7 @@ describe JsRegex do
   end
 
   describe '#to_h' do
-    let(:return_value) { described_class.new(//).to_h }
+    let(:return_value) { JsRegex.new(//).to_h }
 
     it 'returns a Hash' do
       expect(return_value).to be_instance_of(Hash)
@@ -22,14 +22,14 @@ describe JsRegex do
   end
 
   describe '#to_json' do
-    let(:return_value) { described_class.new(//).to_json }
+    let(:return_value) { JsRegex.new(//).to_json }
 
     it 'returns a String' do
       expect(return_value).to be_instance_of(String)
     end
 
     it 'encodes the result of #to_h' do
-      js_regex = described_class.new(/[a-z]+/)
+      js_regex = JsRegex.new(/[a-z]+/)
       json = js_regex.to_json
       decoded_json = JSON.parse(json, symbolize_names: true)
       expect(decoded_json).to eq(js_regex.to_h)
@@ -37,32 +37,32 @@ describe JsRegex do
 
     it 'passes on the options parameter, defaulting to {}' do
       expect_any_instance_of(Hash).to receive(:to_json).with(foo: :bar)
-      described_class.new(//).to_json(foo: :bar)
+      JsRegex.new(//).to_json(foo: :bar)
     end
 
     it 'passes on an empty hash as options parameter by default' do
       expect_any_instance_of(Hash).to receive(:to_json).with({})
-      described_class.new(//).to_json
+      JsRegex.new(//).to_json
     end
   end
 
   describe '#to_s' do
     it 'returns a String' do
-      expect(described_class.new(/foo/).to_s).to eq '/foo/'
+      expect(JsRegex.new(/foo/).to_s).to eq '/foo/'
     end
 
     it 'includes options' do
-      expect(described_class.new(/foo/i).to_s).to eq '/foo/i'
+      expect(JsRegex.new(/foo/i).to_s).to eq '/foo/i'
     end
 
     it 'returns /(?:)/ if the source is empty, as `//` is illegal in JS' do
-      expect(described_class.new(//).to_s).to eq '/(?:)/'
+      expect(JsRegex.new(//).to_s).to eq '/(?:)/'
     end
   end
 
   describe '#warnings' do
     it 'returns an Array' do
-      expect(described_class.new(//).warnings).to be_instance_of(Array)
+      expect(JsRegex.new(//).warnings).to be_instance_of(Array)
     end
   end
 end
