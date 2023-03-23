jaynetics/js_regex

commit ba77cc4590e413ea83d12b2dd0615d9a0b8d876b

Performance improvement ...
... rebuild character set from scratch in fewer cases, e.g. don't rebuild `[\-]`

lib/js_regex/converter/set_converter.rb
@@ -17,54 +17,70 @@ class JsRegex
       private
 
       def convert_data
-        return pass_through_with_escaping if directly_compatible?
-
-        content = CharacterSet.of_expression(expression)
-        if expression.case_insensitive? && !context.case_insensitive_root
-          content = content.case_insensitive
-        elsif !expression.case_insensitive? && context.case_insensitive_root
-          warn_of_unsupported_feature('nested case-sensitive set')
-        end
-
-        if context.es_2015_or_higher?
-          context.enable_u_option if content.astral_part?
-          content.to_s(format: 'es6', in_brackets: true)
-        else
-          content.to_s_with_surrogate_ranges
-        end
+        simple_conversion || full_recalculation
       end
 
-      def directly_compatible?
-        all_children_directly_compatible? && !casefolding_needed?
-      end
+      def simple_conversion
+        return false if casefolding_needed?
 
-      def all_children_directly_compatible?
-        # note that #each_expression is recursive
-        expression.each_expression.all? { |ch| child_directly_compatible?(ch) }
-      end
+        result = "[#{'^' if expression.negative?}".dup
 
-      def child_directly_compatible?(exp)
-        case exp.type
-        when :literal
-          # surrogate pair substitution needed on ES2009 if astral
-          exp.text.ord <= 0xFFFF || context.enable_u_option
-        when :set
-          # conversion needed for nested sets, intersections
-          exp.token.equal?(:range)
-        when :type
-          TypeConverter.directly_compatible?(exp)
-        when :escape
-          EscapeConverter::ESCAPES_SHARED_BY_RUBY_AND_JS.include?(exp.token)
+        expression.expressions.each do |subexp|
+          return unless child_res = simple_convert_child(subexp)
+
+          result << child_res.to_s
         end
+
+        result << ']'
       end
 
       def casefolding_needed?
         expression.case_insensitive? ^ context.case_insensitive_root
       end
 
-      def pass_through_with_escaping
-        string = expression.to_s(:base)
-        LiteralConverter.escape_incompatible_bmp_literals(string)
+      def simple_convert_child(exp)
+        case exp.type
+        when :literal
+          return false if !context.u? &&
+            exp.text =~ LiteralConverter::ASTRAL_PLANE_CODEPOINT_PATTERN &&
+            !context.enable_u_option
+
+          LiteralConverter.escape_incompatible_bmp_literals(exp.text)
+        when :set
+          # full conversion is needed for nested sets and intersections
+          exp.token.equal?(:range) && exp.expressions.map do |op|
+            simple_convert_child(op) or return false
+          end.join('-')
+        when :type
+          TypeConverter.directly_compatible?(exp, context) &&
+            exp.text
+        when :escape
+          case exp.token
+          when *CONVERTIBLE_ESCAPE_TOKENS
+            EscapeConverter.new.convert(exp, context)
+          when :literal
+            exp.char.ord <= 0xFFFF &&
+              LiteralConverter.escape_incompatible_bmp_literals(exp.char)
+          end
+        end
+      end
+
+      CONVERTIBLE_ESCAPE_TOKENS = %i[control meta_sequence bell escape octal] +
+        EscapeConverter::ESCAPES_SHARED_BY_RUBY_AND_JS
+
+      def full_recalculation
+        content = CharacterSet.of_expression(expression)
+        if expression.case_insensitive? && !context.case_insensitive_root
+          content = content.case_insensitive
+        elsif !expression.case_insensitive? && context.case_insensitive_root
+          warn_of_unsupported_feature('nested case-sensitive set')
+        end
+        if context.es_2015_or_higher?
+          context.enable_u_option if content.astral_part?
+          content.to_s(format: 'es6', in_brackets: true)
+        else
+          content.to_s_with_surrogate_ranges
+        end
       end
     end
   end

lib/js_regex/converter/context.rb
@@ -40,6 +40,10 @@ class JsRegex
         required_options_hash['u'] = true
       end
 
+      def u?
+        required_options_hash['u']
+      end
+
       def required_options
         required_options_hash.keys
       end

lib/js_regex/converter/literal_converter.rb
@@ -6,11 +6,12 @@ class JsRegex
     # Template class implementation.
     #
     class LiteralConverter < JsRegex::Converter::Base
-      class << self
-        ASTRAL_PLANE_CODEPOINT_PATTERN = /[\u{10000}-\u{10FFFF}]/
+      ASTRAL_PLANE_CODEPOINT_PATTERN = /[\u{10000}-\u{10FFFF}]/
+      LITERAL_REQUIRING_ESCAPE_PATTERN = /[\/\f\n\r\t\v]/
 
+      class << self
         def convert_data(data, context)
-          if data =~ ASTRAL_PLANE_CODEPOINT_PATTERN
+          if !context.u? && data =~ ASTRAL_PLANE_CODEPOINT_PATTERN
             if context.enable_u_option
               escape_incompatible_bmp_literals(data)
             else
@@ -23,7 +24,7 @@ class JsRegex
 
         def convert_astral_data(data)
           data.each_char.each_with_object(Node.new) do |char, node|
-            if char =~ ASTRAL_PLANE_CODEPOINT_PATTERN
+            if char.ord > 0xFFFF
               node << surrogate_substitution_for(char)
             else
               node << escape_incompatible_bmp_literals(char)
@@ -31,8 +32,12 @@ class JsRegex
           end
         end
 
+        ESCAPES = Hash.new { |h, k| raise KeyError, "#{h}[#{k.inspect}]" }
+          .merge("\f\n\r\t\v".chars.to_h { |c| [c, Regexp.escape(c)] })
+          .merge('/' => '\\/')
+
         def escape_incompatible_bmp_literals(data)
-          data.gsub('/', '\\/').gsub(/[\f\n\r\t]/) { |lit| Regexp.escape(lit) }
+          data.gsub(LITERAL_REQUIRING_ESCAPE_PATTERN, ESCAPES)
         end
 
         private

lib/js_regex/converter/type_converter.rb
@@ -13,7 +13,7 @@ class JsRegex
       ES2018_XGRAPHEME_EXPANSION = '[\P{M}\P{Lm}](?:(?:[\u035C\u0361]\P{M}\p{M}*)|\u200d|\p{M}|\p{Lm}|\p{Emoji_Modifier})*'
       LINEBREAK_EXPANSION        = '(?:\r\n|[\n\v\f\r\u0085\u2028\u2029])'
 
-      def self.directly_compatible?(expression)
+      def self.directly_compatible?(expression, _context = nil)
         case expression.token
         when :space, :nonspace
           !expression.ascii_classes?

commit e8b38109e0cc04b2cd7c1b9a696a2b4c429edec9

Keep set-specific escapes
fixes regression introduced in ba77cc4

closes #19

lib/js_regex/converter/set_converter.rb
@@ -55,6 +55,8 @@ class JsRegex
           TypeConverter.directly_compatible?(exp, context) &&
             exp.text
         when :escape
+          return exp.text if SET_SPECIFIC_ESCAPES_PATTERN.match?(exp.text)
+
           case exp.token
           when *CONVERTIBLE_ESCAPE_TOKENS
             EscapeConverter.new.convert(exp, context)
@@ -65,6 +67,7 @@ class JsRegex
         end
       end
 
+      SET_SPECIFIC_ESCAPES_PATTERN = /[\^\-]/
       CONVERTIBLE_ESCAPE_TOKENS = %i[control meta_sequence bell escape octal] +
         EscapeConverter::ESCAPES_SHARED_BY_RUBY_AND_JS
 

spec/lib/js_regex/converter/set_converter_spec.rb
@@ -142,6 +142,10 @@ describe JsRegex::Converter::SetConverter do
     expect(/[^x\b]/).to stay_the_same.and keep_matching("a\bz", with_results: %w[a z])
   end
 
+  it 'preserves escapes that are specific to sets' do
+    expect(/[\^x\-y]/).to stay_the_same.and keep_matching("^x-y.", with_results: %w[^ x - y])
+  end
+
   it 'converts forward slashes to forward slash escapes' do
     expect(%r{[/]}).to\
     become(%r{[\/]}).and keep_matching('/')